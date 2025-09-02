851744238f6476ba228eb17a4297aa347d33cef5
Riya Patel <riyaptl3473@gmail.com>
2025-09-02 20:08:31 +0530

    Fix: auth api correction

--- src/service/authService.jsx
+++ src/service/authService.jsx
@@ -4,12 +4,12 @@ const API_URL = `${baseURL}/auth`;
 // const API_URL = "http://localhost:5000/api/auth/";
 
 export const signupUser = async (data) => {
-  const res = await axios.post(API_URL + "signup", data);
+  const res = await axios.post(`${API_URL}/signup`, data);
   return res.data;
 };
 
 export const loginUser = async (data) => {
-  const res = await axios.post(API_URL + "login", data);
+  const res = await axios.post(`${API_URL}/login`, data);
   if (res.data.token) {
     localStorage.setItem("token", res.data.token);
   } 
